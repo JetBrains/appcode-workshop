6bdccd2997186f8b586f7d91eb8f1c3e590fb5a0
Anton Makeev <Anton.Makeev@jetbrains.com>
2015-12-21 13:41:30 +0100

    fixes and updates for Swift

---
+++ 01 - Navigation/06 - Structure tool window/6.2. Structure in Swift files.swift
@@ -0,0 +1,31 @@
+/**
+* File Structure Tool Window
+*
+* Displays outline of file: classes, functions (with icons displaying accessibility).
+* Also shows HTML, JavaScript, CSS, ...
+*
+* Command+7
+* Command+F12 for a popup
+*
+* 1. Open the File Structure Tool Window.
+* 2. Using speed search navigate to protocolMethod
+
+* 3. Open the File Structure popup.
+* 4. Using speed search navigate to protocolMethod
+*/
+
+
+let answer = "42";
+
+protocol AnswerMachine {
+    func protocolMethod();
+}
+
+class AnswerMachineV1 : AnswerMachine {
+    func askQuestion(question: String) -> String {
+        return answer;
+    }
+
+    func protocolMethod() {
+    }
+}
