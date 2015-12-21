9856194f91d46a05b38b1628fbac8a4e46e8cc9b
Anton Makeev <Anton.Makeev@jetbrains.com>
2015-12-21 14:47:12 +0100

    fixes and updates for Swift

---
+++ 02 - Editing/06 - Generate code/6.2. Override-Implement in Swift.swift
@@ -0,0 +1,25 @@
+/**
+* Override/Implement
+*
+* Quickly generate stubs for methods inherited from protocols and super classes.
+*
+* Control+O - Override super classes' methods
+* Control+I - Implement protocols' methods
+* Command+N - Show 'Generate' menu
+*/
+
+
+protocol P {
+    func protocolMethod1();
+    func protocolMethod2();
+}
+
+class Super {
+    func superMethod();
+}
+
+class Child : Super, P {
+    // 1. Place the caret here
+    // 2. Invoke 'Implement Methods' action; note that all methods from the protocols are selected automatically.
+    // 3. Invoke 'Override Merhods' action. Use speed seach to find and implement protocolMethod1
+}
