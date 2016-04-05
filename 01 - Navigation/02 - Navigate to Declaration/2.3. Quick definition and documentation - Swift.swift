47f3ae03b26932d7f6d3c988ef06ee95c8a32fe2
Stanislav Dombrovsky <stanislav.dombrovsky@jetbrains.com>
2016-04-05 20:31:35 +0300

    Add showcase for Quick documentation in Swift

---
+++ 01 - Navigation/02 - Navigate to Declaration/2.3. Quick definition and documentation - Swift.swift
@@ -0,0 +1,15 @@
+//
+// Created by jetbrains on 05/04/16.
+// Copyright (c) 2016 JetBrains s.r.o. All rights reserved.
+//
+
+import Foundation
+
+func quickDefinition() {
+    let user = SwiftUser(name:"name")
+
+
+    // 1. Place the caret on celebrateBirthday and see a quick definition
+    // 2. Place the caret on celebrateBirthday and see a quick documentation.
+    user.celebrateBirthday()
+}
